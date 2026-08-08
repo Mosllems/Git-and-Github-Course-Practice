2b58b0ab392301e15beb167772a59ee8b541285e
Moslem Amiri <moslemamiri82@gmail.com>
2026-08-08 18:31:15 +0330

    add family name in file4.py

--- file4.py
+++ file4.py
@@ -1,5 +1,5 @@
 def greet(**kwargs):
-    return f"Hello {kwargs.get('name')}!"
+    return f"Hello from {kwargs.get('name')} {kwargs.get('family_name')}!"
 
 
-print(greet(name="Moslem"))
+print(greet(name="Moslem", family_name="Amiri"))
