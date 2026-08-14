022e4a3422e05fbc98fb260d0fda0df5c108742b
Moslem Amiri <moslemamiri82@gmail.com>
2026-08-14 22:35:33 +0330

    add student class

---
+++ student.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
+
+
+person = Person("Moslem", "Amiri")
+
+print(person.fname, person.lname)
